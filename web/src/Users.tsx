63cb7be1818cd036bc919d7f1d3511084154c77a
benzo-y <fukubenzo@gmail.com>
2023-02-28 15:18:31 +0900

    call POST and DELETE api from web

--- web/src/Users.tsx
+++ web/src/Users.tsx
@@ -14,12 +14,55 @@ const Users: React.FC<{}> = () => {
     (async() => {
       const res = await fetch(`/users/${userId.current.value}`)
       const data = await res.json()
-      console.log(data);
       setData(() => JSON.stringify(data) );
     })()
   }
 
+  async function postData(path: string, body: any) {
+    const res =  await fetch(path, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    })
+    const data = await res.text();
+    setData(() => data );
+  }
+
+  const createUser = () => {
+    postData('/users', 
+    { 
+      "username": username.current.value,
+      "email": email.current.value,
+    }
+  )}
+
+  const updateUser = () => {
+    postData(`/users/${updateuUerId.current.value}`, 
+    { 
+      "username": updateUserName.current.value,
+      "email": updateEmail.current.value,
+    }
+  )}
+
+  const deleteUser = () => {
+    (async() => {
+      const res =  await fetch(`users/${deleteUserId.current.value}`, {
+        method: 'DELETE',
+      })
+      const data = await res.text();
+      setData(() => data );
+      })();
+  }
+
   const userId = useRef<HTMLInputElement>(null!);
+  const username = useRef<HTMLInputElement>(null!);
+  const email = useRef<HTMLInputElement>(null!);
+  const updateuUerId = useRef<HTMLInputElement>(null!);
+  const updateUserName = useRef<HTMLInputElement>(null!);
+  const updateEmail = useRef<HTMLInputElement>(null!);
+  const deleteUserId = useRef<HTMLInputElement>(null!);
 
 
   return (
@@ -28,6 +71,21 @@ const Users: React.FC<{}> = () => {
       <div>
         <button onClick={userById}>Get User By ID</button>
         <input ref={userId} type="number"></input>
+      </div>
+      <div>
+        <button onClick={createUser}>Create User</button>
+        Name:<input ref={username} type="text"></input>
+        E-Mail:<input ref={email} type="mail"></input>
+      </div>
+      <div>
+        <button onClick={updateUser}>Update User</button>
+        ID:<input ref={updateuUerId} type="number"></input>
+        Name:<input ref={updateUserName} type="text"></input>
+        E-Mail:<input ref={updateEmail} type="mail"></input>
+      </div>
+      <div>
+        <button onClick={deleteUser}>Delete User By ID</button>
+        <input ref={deleteUserId} type="number"></input>
       </div>
       <div>Result: {data}</div>
     </div>
