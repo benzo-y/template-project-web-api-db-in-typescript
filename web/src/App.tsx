65fe69a1e6d0738efa41a9ff9bb56ebce34f2a9f
benzo-y <fukubenzo@gmail.com>
2023-02-28 11:19:11 +0900

    add web container

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+        <p>
+          Hello React.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
